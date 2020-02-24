c1378a9360c6950d3d8088428016bc1e71b2102d
Moe <github@m03.ca>
2020-02-24 12:11:00 -0800

    Update VPNHandler.swift

--- ios/Classes/VPNHandler.swift
+++ ios/Classes/VPNHandler.swift
@@ -99,7 +99,7 @@ func connectVPN(result: FlutterResult, usrname: NSString, pwd: NSString, add: NS
             kcs.save(key: "sharedSecretReference", value: "shinobi")
             p.passwordReference = kcs.load(key: "password")
             p.authenticationMethod = NEVPNIKEAuthenticationMethod.sharedSecret
-            p.sharedSecretReference = kcs.getData("sharedSecretReference")
+            p.sharedSecretReference = kcs.load(key: "sharedSecretReference")
             p.useExtendedAuthentication = true
             p.disconnectOnSleep = false
 
